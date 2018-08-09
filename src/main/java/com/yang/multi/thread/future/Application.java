b69cd0a9b526fd1711b959994a93a10986ce5860
544759698 <544759698@qq.com>
2018-08-09 23:04:58 +0800

    multi

---
+++ src/main/java/com/yang/multi/thread/future/Application.java
@@ -0,0 +1,7 @@
+package com.yang.multi.thread.future;
+
+/**
+ * Created by Administrator on 2018/7/31.
+ */
+public class Application {
+}
---
+++ src/main/java/com/yang/multi/thread/producerconsumer/Application.java
@@ -0,0 +1,7 @@
+package com.yang.multi.thread.producerconsumer;
+
+/**
+ * Created by Administrator on 2018/7/31.
+ */
+public class Application {
+}
